e81923040c6405b5acc03883ccde2a0e066f88fd
Johnny Nieves <50960412+johnnynieves@users.noreply.github.com>
2019-09-06 17:17:42 -0700

    Initial start of default printer program

---
+++ printer_setup.py
@@ -0,0 +1,17 @@
+from getpass import getpass
+from telnetlib import Telnet
+
+
+HOST = "10.0.0.109"
+user = input("Username ")
+password = getpass("Password ")
+
+
+with open("setup.txt","r") as f:
+    config = f.readlines()
+with Telnet(host=HOST) as tn:    
+    for line in config:
+        #print(line)
+        tn.write(line.encode('ascii'))
+    print(tn.read_all().decode('ascii'))
+
